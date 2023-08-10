23ed178ee63e1371110651009cfb465373116ed0
sinan7git <sinanaslam2003@gmail.com>
2023-08-10 22:55:26 +0530

    'test'

--- backendFood/urls.py
+++ backendFood/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/food/', include("api.v1.food.urls")),
     path('api/v1/auth/' , include('api.auth.urls')),
-    path('',TemplateView.as_view(template_name="index.html"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
